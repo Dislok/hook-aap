127c89a7a4c7341cd538dec893ce106290c23dd9
Dislo <david_ibavit@hotmail.com>
2023-05-31 17:05:58 -0600

    ult

--- src/hooks/useFetchs.js
+++ src/hooks/useFetchs.js
@@ -3,6 +3,9 @@ import { useEffect } from "react"
 
 export const useFetchs = (url) => {
 
+    const [state, setState] = useState({
+      data :   
+    })
     const getFetch = async() =>{
        const resp = await fetch(url);
        const data = await resp.json();
